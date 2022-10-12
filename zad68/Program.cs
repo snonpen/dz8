c48da753a1ce4f79990acc0c5e9b34aaae93296e
snonpen <112386250+snonpen@users.noreply.github.com>
2022-10-12 15:49:52 +0300

    first commit

---
+++ zad68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+{
+int AkkermanFunc(int m, int n)
+		{
+			if(m == 0)
+			{
+				return n + 1;
+			}
+			if(m > 0 && n == 0)
+			{
+				return AkkermanFunc(m - 1, 1);
+			}
+			return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+		}
+		
+		Console.WriteLine(AkkermanFunc(2,3));
+		Console.WriteLine(AkkermanFunc(3,2));
+}
